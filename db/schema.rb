42427bf89ea17780155bc45e9508a7be66b1f57e
memune <ordinary0826@gmail.com>
2015-10-03 16:00:55 +0000

    carrierwave gem 사용 준비

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822150558) do
+ActiveRecord::Schema.define(version: 20151003155620) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20150822150558) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
